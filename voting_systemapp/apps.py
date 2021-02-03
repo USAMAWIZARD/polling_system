cafa00c0f68252d976a8971906beb25eb008e2eb
USAMAWIZARD <usamawizard@gmail.com>
2021-02-03 12:38:58 +0530

    basic UI created

---
+++ voting_systemapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VotingSystemappConfig(AppConfig):
+    name = 'voting_systemapp'
---
+++ voting_systemapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+voters={}
+candidate={}
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+def def(request):
+    pass
